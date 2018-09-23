6c7898d6cd533932dd802a9070fe9b5e79309fd6
Liam Gensel <liamgens@gmail.com>
2018-09-23 16:21:50 -0400

    #5 Clear button functionality added.

--- src/renderer/components/Toolbar.vue
+++ src/renderer/components/Toolbar.vue
@@ -2,7 +2,7 @@
   <div>
     <v-btn @click="run" color="success"><v-icon light>play_circle_filled</v-icon></v-btn>
     <v-btn color="error"><v-icon light>stop</v-icon></v-btn>
-    <v-btn color="warning"><v-icon light>clear</v-icon></v-btn>
+    <v-btn @click="clear" color="warning"><v-icon light>clear</v-icon></v-btn>
     <v-btn color="blue-grey"><v-icon light>cloud_upload</v-icon></v-btn>
   </div>
 </template>
@@ -12,6 +12,9 @@ export default {
   methods: {
     run: function () {
       this.$store.commit('editFileContents', this.$store.getters.code)
+    },
+    clear: function () {
+      this.$store.commit('editFileContents', '')
     }
   }
 }
