53325d90ed7e9b9f35b3355bb69bbf13a1451d4b
Liam Gensel <liamgens@gmail.com>
2018-10-18 13:49:45 -0400

    #43 Syntax Checker added with tests and instruction metadata. Update of parser tests too.

--- test/unit/specs/parser.spec.js
+++ test/unit/specs/parser.spec.js
@@ -2,12 +2,12 @@ import { parse } from '../../../src/bvm/parser'
 
 describe('parser.js', () => {
   it('should remove all whitespace and add each argument to an array of strings', () => {
-    expect(parse('   test    2  ')).eql(['test', '2'])
+    expect(parse('   test    2  ')).to.eql(['test', '2'])
   })
   it('should return an empty array when there is only whitespace', () => {
-    expect(parse('               ')).eql([])
+    expect(parse('               ')).to.eql([])
   })
   it('should return an empty array when there is an empty string', () => {
-    expect(parse('')).eql([])
+    expect(parse('')).to.eql([])
   })
 })
---
+++ test/unit/specs/syntax.spec.js
@@ -0,0 +1,26 @@
+import { syntax } from '../../../src/bvm/syntax'
+
+describe('syntax.js', () => {
+  it('should throw an error when there are no arguments', () => {
+    expect(() => syntax([])).to.throw('Instruction not found.')
+  })
+  it('should throw an error when there are too little arguments', () => {
+    expect(() => syntax(['add', 1, 5])).to.throw('Expected 3 args but got 2')
+  })
+  it('should throw an argument when there are too many arguments', () => {
+    expect(() => syntax(['add', 34, -92, 23, 'd'])).to.throw(
+      'Expected 3 args but got 4'
+    )
+  })
+  it('should throw an error when the instruction does not exist', () => {
+    expect(() => syntax(['spaget', 55, 2, 'd'])).to.throw(
+      'Instruction not found.'
+    )
+  })
+  it('should return an object containing the instruction and the arguments', () => {
+    expect(syntax(['add', -9, 200, 'd'])).to.eql({
+      inst: 'add',
+      args: [-9, 200, 'd']
+    })
+  })
+})
