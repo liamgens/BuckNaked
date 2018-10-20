c5a11683718f654e3ac121220132256298d02255
Liam Gensel <liamgens@gmail.com>
2018-10-20 13:42:05 -0400

    #11 Environment class added for use in the interpreter and executor to keep track of scoping and current execution

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,15 +12,21 @@ module.exports = {
   globals: {
     __static: true
   },
-  plugins: [
-    'html'
-  ],
-  'rules': {
+  plugins: ['html'],
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+  },
+  overrides: [
+    {
+      files: '*.spec.js',
+      rules: {
+        'no-unused-expressions': 'off'
+      }
+    }
+  ]
 }
---
+++ src/bvm/environment.js
@@ -0,0 +1,48 @@
+export class Environment {
+  constructor (parentEnv) {
+    this.scope = parentEnv.scope
+    this.functions = parentEnv.functions
+  }
+  getScope () {
+    return this.scope
+  }
+  getFunctions () {
+    return this.functions
+  }
+  addVariable (name, value) {
+    value = value || null
+    if (name in this.scope) {
+      throw new Error(`Variable ${name} is already defined.`)
+    } else if (name in this.functions) {
+      throw new Error(`Variable ${name} is already defined as a function.`)
+    }
+    this.scope[name] = value
+  }
+  setVariable (name, value) {
+    if (name in this.scope) {
+      this.scope[name] = value
+    } else {
+      throw new Error(`Variable ${name} not recognized.`)
+    }
+  }
+  getVariable (name) {
+    if (name in this.scope) {
+      return this.scope[name]
+    }
+    throw new Error(`Variable ${name} not recognized.`)
+  }
+  addFunction (name, code) {
+    if (name in this.functions) {
+      throw new Error(`Function ${name} already exists.`)
+    } else if (name in this.scope) {
+      throw new Error(`Variable ${name} already exists.`)
+    }
+    this.functions[name] = code
+  }
+  getFunction (name) {
+    if (name in this.functions) {
+      return this.functions[name]
+    }
+    throw new Error(`Function ${name} not recognized.`)
+  }
+}
