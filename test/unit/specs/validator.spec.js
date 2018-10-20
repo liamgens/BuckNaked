f0086c2ec1f741101b02e7619ea20976fb5e752a
Brendan McMahon <bmmcmaho@buffalo.edu>
2018-10-20 15:08:44 -0400

    #44 finished validator by adding code to get the types of stored variables when they are used in expressions, and adding tests for this

--- test/unit/specs/validator.spec.js
+++ test/unit/specs/validator.spec.js
@@ -1,9 +1,10 @@
 import { typecheck, getArgTypes } from '../../../src/bvm/validator'
+import { Environment } from '../../../src/bvm/environment'
 
 describe('validator.js', () => {
   it('should return an array with three num types', () => {
     let expected = ['num', 'num', 'num']
-    let actual = getArgTypes(['1.20', '1.0', '1004.1'])
+    let actual = getArgTypes(['1.20', '1.0', '1004.1'], {})
     for (let i = 0; i < expected.length; i++) {
       expect(actual[i]).eql(expected[i])
     }
@@ -11,7 +12,7 @@ describe('validator.js', () => {
 
   it('should return a set containing only bool type', () => {
     let expected = ['bool', 'bool', 'bool']
-    let actual = getArgTypes(['true', 'false', 'false'])
+    let actual = getArgTypes(['true', 'false', 'false'], {})
     for (let i = 0; i < expected.length; i++) {
       expect(actual[i]).eql(expected[i])
     }
@@ -19,7 +20,29 @@ describe('validator.js', () => {
 
   it('should return a set containing only string type', () => {
     let expected = ['string', 'string', 'string']
-    let actual = getArgTypes(['"oink"', '"spoughite"', '"Farmer McNabb"'])
+    let actual = getArgTypes(['"oink"', '"spoughite"', '"Farmer McNabb"'], {})
+    for (let i = 0; i < expected.length; i++) {
+      expect(actual[i]).eql(expected[i])
+    }
+  })
+
+  it('getArgTypes should return a set of the types of the values of the variables passed in', () => {
+    let expected = ['string', 'string', 'num', 'bool']
+    let env = new Environment({ scope: { }, functions: { } })
+    env.addVariable('string1', '"SlickRick"')
+    env.addVariable('string2', '"SlackRack"')
+    env.addVariable('num1', '69.420')
+    env.addVariable('BrendanIsHandsome', 'false')
+    let actual = getArgTypes(['string1', 'string2', 'num1', 'BrendanIsHandsome'], env)
+    for (let i = 0; i < expected.length; i++) {
+      expect(actual[i]).eql(expected[i])
+    }
+  })
+
+  it('should return \'name\' when a name that has not been assigned yet is passed in', () => {
+    let expected = ['name', 'name']
+    let env = new Environment({ scope: { }, functions: { } })
+    let actual = getArgTypes(['string1', 'string2'], env)
     for (let i = 0; i < expected.length; i++) {
       expect(actual[i]).eql(expected[i])
     }
@@ -30,7 +53,7 @@ describe('validator.js', () => {
       inst: 'add',
       args: ['1.5', 'false', 'dest']
     }
-    expect(function () { typecheck(instSet) }).throw('Operation and operand types did not match')
+    expect(function () { typecheck(instSet, {}) }).throw('Operation and operand types did not match')
   })
 
   it('should throw an error when operand types do not match operator types', () => {
@@ -38,6 +61,14 @@ describe('validator.js', () => {
       inst: 'add',
       args: ['1.5', 'false', 'dest']
     }
-    expect(function () { typecheck(instSet) }).throw('Operation and operand types did not match')
+    expect(function () { typecheck(instSet, {}) }).throw('Operation and operand types did not match')
+  })
+
+  it('should throw an error when operand types do not match operator types', () => {
+    let instSet = {
+      inst: 'add',
+      args: ['1.5', 'false', 'dest']
+    }
+    expect(function () { typecheck(instSet, {}) }).throw('Operation and operand types did not match')
   })
 })
